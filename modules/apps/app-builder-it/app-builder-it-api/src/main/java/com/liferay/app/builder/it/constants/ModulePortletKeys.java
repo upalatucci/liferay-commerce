e7aa0380b0d4d76d6930e8171b687a9e81888af5
Marco Leo <marco.leo@liferay.com>
2021-04-25 22:19:18 +0200

    LOWCODE-0 generic prefix. as each portlet will be registered with :  prefix + module name

---
+++ modules/apps/app-builder-it/app-builder-it-api/src/main/java/com/liferay/app/builder/it/constants/ModulePortletKeys.java
@@ -0,0 +1,6 @@
+package com.liferay.app.builder.it.constants;
+
+public class ModulePortletKeys {
+	public static final String MODULE_ADMIN_PREFIX =
+		"com_liferay_app_builder_it_web_internal_portlet_";
+}
