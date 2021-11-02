73e5ae7b5a7f46814ff2c55b18b44f1d04cd5f81
Ugo Palatucci <upalatucci@gmail.com>
2021-11-02 11:57:39 +0100

    COMMERCE-7143 SaveTemplate refactoring

---
+++ modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/SaveTemplate.js
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import {useModal} from '@clayui/modal';
+import PropTypes from 'prop-types';
+import React, {useCallback, useEffect, useState} from 'react';
+
+import SaveTemplateModal from './SaveTemplateModal';
+
+function SaveTemplate({
+	formSaveAsTemplateDataQuerySelector,
+	formSaveAsTemplateURL,
+	portletNamespace,
+}) {
+	const [disable, setDisable] = useState(true);
+	const [visible, setVisible] = useState(false);
+	const {observer, onClose} = useModal({
+		onClose: () => setVisible(false),
+	});
+	const onButtonClick = useCallback(() => {
+		setVisible(true);
+	}, [setVisible]);
+
+	useEffect(() => {
+		const externalInput = document.querySelector(
+			`#${portletNamespace}internalClassName`
+		);
+
+		if (!externalInput) {
+			setDisable(false);
+
+			return;
+		}
+
+		function handleExternalInputChange() {
+			setDisable(false);
+		}
+
+		externalInput.addEventListener('change', handleExternalInputChange);
+
+		return () =>
+			externalInput.removeEventListener(
+				'change',
+				handleExternalInputChange
+			);
+	}, []);
+
+	return (
+		<span className="mr-3">
+			<ClayButton
+				disabled={disable}
+				displayType="secondary"
+				id={`${portletNamespace}saveTemplate`}
+				onClick={onButtonClick}
+				type="button"
+			>
+				{Liferay.Language.get('save-as-template')}
+			</ClayButton>
+
+			{visible && (
+				<SaveTemplateModal
+					closeModal={onClose}
+					formDataQuerySelector={formSaveAsTemplateDataQuerySelector}
+					formSubmitURL={formSaveAsTemplateURL}
+					namespace={portletNamespace}
+					observer={observer}
+				/>
+			)}
+		</span>
+	);
+}
+
+SaveTemplate.propTypes = {
+	formSaveAsTemplateDataQuerySelector: PropTypes.string.isRequired,
+	formSaveAsTemplateURL: PropTypes.string.isRequired,
+	portletNamespace: PropTypes.string.isRequired,
+};
+
+export default SaveTemplate;
---
+++ modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/SaveTemplateModal.js
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import ClayModal from '@clayui/modal';
+import {useIsMounted} from '@liferay/frontend-js-react-web';
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+
+import {saveTemplateAPI} from './api';
+
+const SaveTemplateModal = ({
+	closeModal,
+	formDataQuerySelector,
+	formSubmitURL,
+	namespace,
+	observer,
+}) => {
+	const inputNameId = namespace + 'name';
+	const isMounted = useIsMounted();
+	const [errorMessage, setErrorMessage] = useState();
+	const [loadingResponse, setLoadingResponse] = useState(false);
+	const [inputValue, setInputValue] = useState('');
+
+	const _handleSubmit = async (event) => {
+		event.preventDefault();
+
+		try {
+			const updateData = {[inputNameId]: inputValue};
+			const saveTemplateResponse = saveTemplateAPI(
+				formSubmitURL,
+				formDataQuerySelector,
+				updateData
+			);
+
+			if (isMounted()) {
+				if (saveTemplateResponse.error) {
+					setLoadingResponse(false);
+					setErrorMessage(responseContent?.error);
+				} else {
+					closeModal();
+				}
+			}
+		} catch (error) {
+			setErrorMessage(Liferay.Language.get('unexpected-error'));
+		} finally {
+			setLoadingResponse(true);
+		}
+	};
+
+	return (
+		<ClayModal observer={observer} size="md">
+			<ClayModal.Header>
+				{Liferay.Language.get('save-as-template')}
+			</ClayModal.Header>
+
+			<form id={`${namespace}form`} onSubmit={_handleSubmit}>
+				<ClayModal.Body>
+					<div
+						className={`form-group ${
+							errorMessage ? 'has-error' : ''
+						}`}
+					>
+						<label className="control-label" htmlFor={inputNameId}>
+							{Liferay.Language.get('name')}
+
+							<span className="reference-mark">
+								<ClayIcon symbol="asterisk" />
+							</span>
+						</label>
+
+						<input
+							autoFocus
+							className="form-control"
+							disabled={loadingResponse}
+							id={inputNameId}
+							name={inputNameId}
+							onChange={(event) =>
+								setInputValue(event.target.value)
+							}
+							placeholder={Liferay.Language.get('template-name')}
+							required
+							type="text"
+							value={inputValue}
+						/>
+
+						{errorMessage && (
+							<div className="form-feedback-item">
+								<ClayIcon
+									className="inline-item inline-item-before"
+									symbol="exclamation-full"
+								/>
+
+								{errorMessage}
+							</div>
+						)}
+					</div>
+				</ClayModal.Body>
+
+				<ClayModal.Footer
+					last={
+						<ClayButton.Group spaced>
+							<ClayButton
+								disabled={loadingResponse}
+								displayType="secondary"
+								onClick={closeModal}
+							>
+								{Liferay.Language.get('cancel')}
+							</ClayButton>
+
+							<ClayButton
+								disabled={
+									loadingResponse || inputValue.length == 0
+								}
+								displayType="primary"
+								type="submit"
+							>
+								{loadingResponse && (
+									<span className="inline-item inline-item-before">
+										<span
+											aria-hidden="true"
+											className="loading-animation"
+										></span>
+									</span>
+								)}
+
+								{Liferay.Language.get('save')}
+							</ClayButton>
+						</ClayButton.Group>
+					}
+				/>
+			</form>
+		</ClayModal>
+	);
+};
+
+SaveTemplateModal.propTypes = {
+	closeModal: PropTypes.func.isRequired,
+	formDataQuerySelector: PropTypes.string.isRequired,
+	formSubmitURL: PropTypes.string.isRequired,
+	namespace: PropTypes.string.isRequired,
+	observer: PropTypes.object.isRequired,
+};
+
+export default SaveTemplateModal;
