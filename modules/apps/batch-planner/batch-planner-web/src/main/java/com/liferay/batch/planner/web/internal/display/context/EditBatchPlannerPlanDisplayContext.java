c5c60a1cb9aad0e1947c576a45e6128374b68ba4
Matija Petanjek <matija.petanjek@liferay.com>
2021-12-01 22:23:39 +0100

    COMMERCE-7560 batch-planner-web - prepare data for frontend

--- modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/display/context/EditBatchPlannerPlanDisplayContext.java
+++ modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/display/context/EditBatchPlannerPlanDisplayContext.java
@@ -15,11 +15,14 @@
 package com.liferay.batch.planner.web.internal.display.context;
 
 import com.liferay.batch.engine.BatchEngineTaskContentType;
+import com.liferay.batch.planner.model.BatchPlannerMapping;
+import com.liferay.batch.planner.model.BatchPlannerPlan;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.SelectOption;
 import com.liferay.petra.string.StringPool;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,13 +31,33 @@ import java.util.stream.Stream;
 
 /**
  * @author Igor Beslic
+ * @author Matija Petanjek
  */
 public class EditBatchPlannerPlanDisplayContext {
 
 	public EditBatchPlannerPlanDisplayContext(
-		Map<String, String> headlessEndpoints) {
+		List<BatchPlannerPlan> batchPlannerPlans,
+		Map<String, String> headlessEndpoints,
+		BatchPlannerPlan selectedBatchPlannerPlan) {
 
 		_headlessEndpoints = Collections.unmodifiableMap(headlessEndpoints);
+
+		if (selectedBatchPlannerPlan == null) {
+			_selectedBatchPlannerMappings = new HashMap<>();
+			_selectedBatchPlannerPlanId = 0;
+			_selectedBatchPlannerPlanName = StringPool.BLANK;
+			_selectedExternalType = StringPool.BLANK;
+		}
+		else {
+			_selectedBatchPlannerMappings = _getSelectedBatchPlannerMappings(
+				selectedBatchPlannerPlan);
+			_selectedBatchPlannerPlanId =
+				selectedBatchPlannerPlan.getBatchPlannerPlanId();
+			_selectedBatchPlannerPlanName = selectedBatchPlannerPlan.getName();
+			_selectedExternalType = selectedBatchPlannerPlan.getExternalType();
+		}
+
+		_templateSelectOptions = _getTemplateSelectOptions(batchPlannerPlans);
 	}
 
 	public List<SelectOption> getExternalTypeSelectOptions() {
@@ -56,6 +79,22 @@ public class EditBatchPlannerPlanDisplayContext {
 		return _headlessEndpoints;
 	}
 
+	public long getSelectedBatchPlannerPlanId() {
+		return _selectedBatchPlannerPlanId;
+	}
+
+	public Map<String, String> getSelectedBatchPlannerPlanMappings() {
+		return _selectedBatchPlannerMappings;
+	}
+
+	public String getSelectedBatchPlannerPlanName() {
+		return _selectedBatchPlannerPlanName;
+	}
+
+	public String getSelectedExternalType() {
+		return _selectedExternalType;
+	}
+
 	public List<SelectOption> getSelectOptions() {
 		Set<Map.Entry<String, String>> entries = _headlessEndpoints.entrySet();
 
@@ -75,6 +114,55 @@ public class EditBatchPlannerPlanDisplayContext {
 		return selectOptions;
 	}
 
+	public List<SelectOption> getTemplateSelectOptions() {
+		return _templateSelectOptions;
+	}
+
+	private Map<String, String> _getSelectedBatchPlannerMappings(
+		BatchPlannerPlan selectedBatchPlannerPlan) {
+
+		Map<String, String> selectedBatchPlannerMappings = new HashMap<>();
+
+		for (BatchPlannerMapping batchPlannerMapping :
+				selectedBatchPlannerPlan.getBatchPlannerMappings()) {
+
+			selectedBatchPlannerMappings.put(
+				batchPlannerMapping.getInternalFieldName(),
+				batchPlannerMapping.getExternalFieldName());
+		}
+
+		return selectedBatchPlannerMappings;
+	}
+
+	private List<SelectOption> _getTemplateSelectOptions(
+		List<BatchPlannerPlan> batchPlannerPlans) {
+
+		List<SelectOption> templateSelectOptions = new ArrayList<>();
+
+		for (BatchPlannerPlan batchPlannerPlan : batchPlannerPlans) {
+			boolean selected = false;
+
+			if (batchPlannerPlan.getBatchPlannerPlanId() ==
+					_selectedBatchPlannerPlanId) {
+
+				selected = true;
+			}
+
+			templateSelectOptions.add(
+				new SelectOption(
+					batchPlannerPlan.getName(),
+					String.valueOf(batchPlannerPlan.getBatchPlannerPlanId()),
+					selected));
+		}
+
+		return templateSelectOptions;
+	}
+
 	private final Map<String, String> _headlessEndpoints;
+	private final Map<String, String> _selectedBatchPlannerMappings;
+	private final long _selectedBatchPlannerPlanId;
+	private final String _selectedBatchPlannerPlanName;
+	private final String _selectedExternalType;
+	private final List<SelectOption> _templateSelectOptions;
 
 }
