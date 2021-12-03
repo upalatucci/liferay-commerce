4334f9953c2872fe90f22b999200a2299459a33d
Matija Petanjek <matija.petanjek@liferay.com>
2021-12-03 14:49:41 +0100

    COMMERCE-7560 batch-planner-service - get related mappings and policies from the entity

--- modules/apps/batch-planner/batch-planner-service/src/main/java/com/liferay/batch/planner/model/impl/BatchPlannerPlanImpl.java
+++ modules/apps/batch-planner/batch-planner-service/src/main/java/com/liferay/batch/planner/model/impl/BatchPlannerPlanImpl.java
@@ -16,8 +16,10 @@ package com.liferay.batch.planner.model.impl;
 
 import com.liferay.batch.planner.model.BatchPlannerLog;
 import com.liferay.batch.planner.model.BatchPlannerMapping;
+import com.liferay.batch.planner.model.BatchPlannerPolicy;
 import com.liferay.batch.planner.service.BatchPlannerLogLocalServiceUtil;
 import com.liferay.batch.planner.service.BatchPlannerMappingLocalServiceUtil;
+import com.liferay.batch.planner.service.BatchPlannerPolicyLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 
 import java.util.List;
@@ -43,6 +45,20 @@ public class BatchPlannerPlanImpl extends BatchPlannerPlanBaseImpl {
 	public List<BatchPlannerMapping> getBatchPlannerMappings() {
 		return BatchPlannerMappingLocalServiceUtil.getBatchPlannerMappings(
 			getBatchPlannerPlanId());
+	}
+
+	@Override
+	public List<BatchPlannerPolicy> getBatchPlannerPolicies() {
+		return BatchPlannerPolicyLocalServiceUtil.getBatchPlannerPolicies(
+			getBatchPlannerPlanId());
+	}
+
+	@Override
+	public BatchPlannerPolicy getBatchPlannerPolicy(String name)
+		throws PortalException {
+
+		return BatchPlannerPolicyLocalServiceUtil.getBatchPlannerPolicy(
+			getBatchPlannerPlanId(), name);
 	}
 
 }
